495fb0eb86527ddcb727bff109407d71d561e495
aswin-kevin <aswinvenkat60@gmail.com>
2023-11-26 01:28:24 +0530

    Delete unused test.js file

--- pages/index.js
+++ pages/index.js
@@ -1,120 +1,93 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import React from "react";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
+export default function SecondHome() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between py-24 px-10 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <code className="font-mono text-lg font-bold">
-            Reduce . Reuse . Recycle
-          </code>
+    <div className="min-h-screen bg-green-100">
+      <header className="text-center py-5 bg-green-500 text-white">
+        <h1 className="text-4xl md:text-5xl font-mono">Cup Care Initiative</h1>
+      </header>
+      <main className="px-5 py-10 md:px-20">
+        <h2 className="text-2xl md:text-3xl mb-5 font-semibold">
+          Paper Cup Recycling Initiative
+        </h2>
+        <p className="mb-5">
+          Welcome to CupCare Initiative, where we&apos;re taking a step towards
+          a greener, more sustainable future. Our latest initiative focuses on
+          the responsible disposal of used paper cups, aiming to make a positive
+          impact on both the environment and the lives of waste collectors in
+          our community.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href=""
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/images/cup-care.png"
-              alt="Cup Care Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <h3 className="text-lg md:text-2xl mb-3 font-semibold">
+          Why Paper Cup Recycling Matters:
+        </h3>
+        <p className="mb-5">
+          <div className="font-mono mb-2">
+            Did you know that a significant number of paper cups end up in
+            landfills every day?
+          </div>
+          <div>
+            By participating in our initiative, you contribute to reducing this
+            environmental impact. Paper cup recycling not only saves trees but
+            also conserves energy compared to manufacturing new cups from raw
+            materials.
+          </div>
+        </p>
+        <h3 className="text-xl md:text-2xl mb-3">How It Works:</h3>
+        <ol className="list-decimal list-inside space-y-2 mb-5">
+          <li>
+            Empty the liquid: Visit our collection point at Research Park mess,
+            empty the liquid in the dedicated funnel.
+          </li>
+          <li>
+            Drop Off: Drop off your used paper cups in the specially designated
+            bin.
+          </li>
+          <li>
+            Make a Difference: Your contribution goes a long way in supporting
+            waste collectors who manually segregate and recycle these cups,
+            diverting them from landfills.
+          </li>
+        </ol>
+        <h3 className="text-xl md:text-2xl mb-3">Benefits of Joining:</h3>
+        <ul className="list-disc list-inside space-y-2 mb-5">
+          <li>
+            Environmental Impact: Reduce your carbon footprint by recycling
+            paper cups, contributing to a cleaner, healthier planet.
+          </li>
+          <li>
+            Community Support: Help local waste collectors by providing them
+            with a source of recyclable material that they can process and sell.
+          </li>
+          <li>
+            Educational Outreach: Raise awareness about the importance of
+            responsible waste disposal and inspire others to join the cause.
+          </li>
+        </ul>
+        <h3 className="text-xl md:text-2xl mb-3">Join Us Today!</h3>
+        <p className="mb-5">
+          Make a positive change by participating in our Paper Cup Recycling
+          Initiative. Every cup you recycle is a step towards a more sustainable
+          future. Spread the word, encourage your friends and colleagues to
+          join, and let&apos;s make a collective impact!
+        </p>
+        <div className="flex items-center justify-center">
+          <p className="mt-3 font-mono text-lg font-semibold">
+            Together, we can make a difference
+          </p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:rounded-full before:bg-gradient-radia before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40">
-        <h1 className="text-black text-4xl font-extrabold font-mono w-full">
-          Let&apos;s Join
-        </h1>
-      </div>
-
-      <div className="flex justify-center px-10">
-        <p className="text-gray-600 font-mono">
-          Our Paper Cup Recycling Initiative!
+        <div className="flex justify-center">
+          <p className="font-mono">one paper cup at a time!</p>
+        </div>
+      </main>
+      <footer className="text-center py-5 bg-green-500 text-white">
+        <p>
+          Visit us at{" "}
+          <a href="https://cupcare.hackwithash.com" className="underline">
+            cupcare.hackwithash.com
+          </a>{" "}
+          for more information and updates on our initiatives.
         </p>
-      </div>
-
-      <div className="my-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </footer>
+    </div>
   );
 }
