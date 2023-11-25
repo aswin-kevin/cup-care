c4750ae4cafacb43d70bb550bbb8a0f1e4bc3420
aswin-kevin <aswinvenkat60@gmail.com>
2023-11-26 00:57:33 +0530

    Fix typo in heading

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
 
       <div className="relative flex place-items-center before:absolute before:rounded-full before:bg-gradient-radia before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40">
         <h1 className="text-black text-4xl font-extrabold font-mono w-full">
-          Let's Join
+          Let&apos;s Join
         </h1>
       </div>
 
